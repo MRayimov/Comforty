7827c5afb3fa792216644ef7c9f8e17b58dc3629
krasavchikjoha <joha.uzbsila2002@gmail.com>
2024-10-08 18:02:42 +0500

    odil

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Carousel } from "@material-tailwind/react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <div className="flex justify-center ">
+        <Carousel className="rounded-xl w-[1400px] h-[500px] bg-gray-200 ">
+          <div className="flex justify-end items-center h-full">
+            <img
+              className="w-[400px] h-[400px]"
+              src="./images/hero/productImage.png"
+              alt=""
+            />
+          </div>
+          <div className="flex justify-end items-center h-full">
+            <img
+              className="w-[400px] h-[400px]"
+              src="https://api.cabinet.smart-market.uz/uploads/images/ff808181a4db7261d393c97e"
+              alt=""
+            />
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
